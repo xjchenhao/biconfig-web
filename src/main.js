9e1fab70eba89472e692cfb15e00a8650c088360
xjchenhao <wy.chenhao@qq.com>
2020-10-29 17:15:04 +0800

    重新init，换vue3

--- src/main.js
+++ src/main.js
@@ -1,9 +1,4 @@
-import Vue from 'vue';
-import App from './App.vue';
+import { createApp } from 'vue'
+import App from './App.vue'
 
-Vue.config.productionTip = false;
-
-new Vue({
-  data: {},
-  render: h => h(App),
-}).$mount('#app');
+createApp(App).mount('#app')
