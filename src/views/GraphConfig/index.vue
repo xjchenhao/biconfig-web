2c5dc1e4d8ef7d440693f370e303657f16ebb59f
xjchenhao <wy.chenhao@qq.com>
2020-10-30 15:59:57 +0800

    增加图表配置页面的基本布局

--- src/views/GraphConfig/index.vue
+++ src/views/GraphConfig/index.vue
@@ -1,10 +1,31 @@
 <template>
-  <div>GraphConfig</div>
+  <div>
+    <a-page-header
+      style="border: 1px solid rgb(235, 237, 240)"
+      title="配置规则"
+      sub-title="条形图的规则配置"
+      @back="() => null"
+    />
+    <a-layout style="flex-direction: row;">
+      <a-layout-content style="flex:1">
+        我是图表
+      </a-layout-content>
+      <a-layout-sider style="width:300px;height:calc(100vh - 66px);overflow: scroll;">
+        我是配置菜单
+      </a-layout-sider>
+    </a-layout>
+  </div>
 </template>
 
 <script>
+import { PageHeader, Layout } from 'ant-design-vue';
 export default {
-
+  components: {
+    aPageHeader: PageHeader,
+    aLayout: Layout,
+    aLayoutSider: Layout.sider,
+    aLayoutContent: Layout.content,
+  },
 };
 </script>
 
