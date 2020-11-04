229609eb0249e6af90b34982eb9547851608335e
xjchenhao <wy.chenhao@qq.com>
2020-11-04 16:08:31 +0800

    增加堆叠分组、区间分组的支持

--- src/views/GraphConfig/GraphConfigDataMap/ColumnDataMap.vue
+++ src/views/GraphConfig/GraphConfigDataMap/ColumnDataMap.vue
@@ -24,16 +24,42 @@
     </a-form-item>
     <a-form-item
       class="graphConfigForm-from-item"
-      name="isGroup"
-      label="是否分组柱形图"
+      label="柱形图类型"
+      required
     >
-      <a-switch v-model:checked="formIsGroup" />
+      <a-select
+        style="width:170px"
+        v-model:value="graphType"
+        @change="renderGraph"
+      >
+        <a-select-option value="">
+          基础柱形图
+        </a-select-option>
+        <a-select-option value="Group">
+          分组柱形图
+        </a-select-option>
+        <a-select-option value="Stack">
+          堆积柱状图
+        </a-select-option>
+        <a-select-option value="Range">
+          区间柱状图
+        </a-select-option>
+      </a-select>
     </a-form-item>
     <a-form-item
-      v-show="form.isGroup"
+      v-show="form.isStack"
+      class="graphConfigForm-from-item"
+      name="isPercent"
+      label="是否百分百柱状图"
+    >
+      <a-switch v-model:checked="form.isPercent" />
+    </a-form-item>
+    <a-form-item
+      v-show="isShowSeriesField"
       class="graphConfigForm-from-item"
       name="seriesField"
       label="关联字段"
+      :required="isShowSeriesField"
     >
       <a-input
         v-model:value="form.seriesField"
@@ -44,13 +70,15 @@
 </template>
 
 <script>
-import { Form, Input, Switch } from 'ant-design-vue';
+import { Form, Input, Switch, Select } from 'ant-design-vue';
 
 export default {
   components: {
     aFormItem: Form.Item,
     aInput: Input,
     aSwitch: Switch,
+    aSelect: Select,
+    aSelectOption: Select.Option,
   },
   emits: [ 'update' ],
   props: {
@@ -67,17 +95,51 @@ export default {
         xField: 'year',
         yField: 'value',
         isGroup: false,
+        isStack: false,
+        isRange: false,
+        isPercent: false,
         seriesField: '',
       },
     };
   },
   computed: {
+    graphType: {
+      get() {
+        if (this.form.isGroup) {
+          return 'Group';
+        }
+        if (this.form.isStack) {
+          return 'Stack';
+        }
+        if (this.form.isRange) {
+          return 'Range';
+        }
+        return '';
+      },
+      set(type) {
+        this.form.isGroup = false;
+        this.form.isStack = false;
+        this.form.isRange = false;
+
+        if (type !== 'Stack') {
+          this.form.isPercent = false;
+        }
+
+        this.form[`is${type}`] = true;
+      },
+    },
+    isShowSeriesField() {
+      return this.form.isGroup || this.form.isStack;
+    },
     formIsGroup: {
       get() {
         return this.form.isGroup;
       },
       set(value) {
-        this.form.seriesField = '';
+
+        if (!this.isShowSeriesField) {
+          this.form.seriesField = '';
+        }
         this.form.isGroup = value;
       },
     },
